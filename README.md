5143b4cf607c46e19cf5da8f6c5e2d41daa33a0e
JimNtexas <grayraven42@gmail.com>
2018-06-12 22:38:18 -0500

    Major gui pieces added, README.md updated

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Pilots use a device called a 'Precision Approach Indicator' (PAPI) to provide a 
 The PAPI uses four lights to display to the pilots in cockpit indications that the airplane is too low, to high, or on the correct glide path.
 
  ![alt text](PAPI_Jersey_Airport.JPG "PAPI Lights")
+ *Image credit - Wikimedia Commons*
  
- This display indicates that the airplane is low relative to the correct glideslope.
+This display indicates that the airplane is low relative to the correct glideslope.
 
-
+There are four possible displays, each with its own meaning.  This app displays one of the possible displays and asks the user to select the correct meaning.
--- app/src/main/java/lander/udemy/grayraven/com/landdecider/MainActivity.kt
+++ app/src/main/java/lander/udemy/grayraven/com/landdecider/MainActivity.kt
@@ -1,13 +1,11 @@
 package lander.udemy.grayraven.com.landdecider
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.IOException
-import android.graphics.drawable.Drawable
 import java.util.*
 
 
@@ -20,13 +18,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         textHeader.setText("Can You Land?")
         papiView.visibility= View.VISIBLE
+        displayPapis()
+        btnRetry.setOnClickListener{
+            displayPapis()
+        }
 
+        btnNo.setOnClickListener{
+            println("No clicked")
+        }
+
+        btnYes.setOnClickListener{
+            println("Yes clicked")
+        }
+    }
+
+
+
+    fun displayPapis() {
         val res = resources
+        @Suppress("DEPRECATION")
         val imageList = arrayListOf<Drawable>(res.getDrawable(R.drawable.tohigh),res.getDrawable(R.drawable.high),
                 res.getDrawable(R.drawable.onglidelslope), res.getDrawable(R.drawable.low), res.getDrawable(R.drawable.pullup))
         val random = Random()
         papiView.setImageDrawable(imageList[random.nextInt(imageList.count())])
-
+        papiView.scaleType = ImageView.ScaleType.FIT_XY
     }
 
 }
